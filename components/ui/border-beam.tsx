1e36023f5a90a6608f835bd123625cb03486d56f
gunsharma617-max <gunsharma617@gmail.com>
2026-09-08 18:26:07 +0530

    Create border-beam.tsx for BorderBeam component

---
+++ components/ui/border-beam.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { BorderBeam } from "border-beam";
+
+export type {
+  BorderBeamProps,
+  BorderBeamSize,
+  BorderBeamTheme,
+  BorderBeamColorVariant,
+} from "border-beam";
+
+export { BorderBeam };
+export default BorderBeam;
